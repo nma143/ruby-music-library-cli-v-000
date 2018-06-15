bf5a4467cb1bab0b9f2f2c507de74ba59e9cb3c1
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-06-15 14:39:07 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -65,7 +65,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_artist
-    puts "Enter an artist:"
+    puts "Please enter the name of an artist:"
     user_input = gets.strip
     
   end
